f72b45cf237a0dff926a6a0e66fca871441c0b9c
Gabriella <gabriella@u.nus.edu>
2025-01-22 17:24:48 +0800

    Add greet and exit function

--- src/main/java/EryzBot.java
+++ src/main/java/EryzBot.java
@@ -1,10 +1,22 @@
 public class EryzBot {
+    static void greet(){
+        String logo = "   ____             ___       __ \n"
+                    + "  / __/_____ _____ / _ )___  / /_\n"
+                    + " / _// __/ // /_ // _  / _ \\/ __/\n"
+                    + "/___/_/  \\_, //__/____/\\___/\\__/ \n"
+                    + "        /___/                    \n";
+        System.out.println("__________________________________________________________\n");
+        System.out.println("Hello, I am \n" + logo);
+        System.out.println("How can I assist you?");
+        System.out.println("__________________________________________________________\n");
+    }
+    static void exit(){
+        System.out.println("Bye! Hope to see you again!");
+        System.out.println("__________________________________________________________");
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
     }
 }
+
