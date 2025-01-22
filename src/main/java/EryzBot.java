95bd8c3ef72cc75bb32ed3759dd42f61a0a84e39
Gabriella <gabriella@u.nus.edu>
2025-01-22 17:46:13 +0800

    Add list function and inputs storage

--- src/main/java/EryzBot.java
+++ src/main/java/EryzBot.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EryzBot {
+    private static ArrayList<String> inputs = new ArrayList<>();
     static void greet(){
         String logo = "   ____             ___       __ \n"
                     + "  / __/_____ _____ / _ )___  / /_\n"
@@ -16,14 +18,23 @@ public class EryzBot {
         System.out.println("Bye! Hope to see you again!");
         System.out.println("__________________________________________________________");
     }
+    static void list(){
+        for (int i = 0; i < inputs.size(); i++) {
+            System.out.println(i+1 + ". " + inputs.get(i));
+        }
+    }
     static void echo(Scanner scanner){
         String input = scanner.nextLine();
-        System.out.println("__________________________________________________________");
         if (input.equalsIgnoreCase("bye")){
             exit();
+        } else if (input.equalsIgnoreCase("list")){
+            list();
+            System.out.println("__________________________________________________________\n");
+            echo(scanner);
         } else {
             System.out.println(input);
-            System.out.println("__________________________________________________________");
+            inputs.add(input);
+            System.out.println("__________________________________________________________\n");
             echo(scanner); 
         }
     }
