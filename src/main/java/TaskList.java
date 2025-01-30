7b610100e47ebe7a56142112b6fabd2a90edbeec
Gabriella <gabriella@u.nus.edu>
2025-01-30 15:16:52 +0800

    Add TaskList

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+    
+    TaskList() {
+        this(new ArrayList<>());
+    }
+    TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+    void add(Task task) {
+        tasks.add(task);
+    }
+    void mark(int index) {
+        tasks.get(index).mark();
+    }
+    void unmark(int index) {
+        tasks.get(index).unmark();
+    }
+}
