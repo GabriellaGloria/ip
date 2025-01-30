2e5780a7df102bbfef384f33d693d0304ee2051e
Gabriella <gabriella@u.nus.edu>
2025-01-30 15:30:59 +0800

    Add Storage

---
+++ src/main/java/Storage.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Storage {
+    private String filepath;
+
+    public Storage(String filepath) {
+        this.filepath = filepath;
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayList<Task> fetch() throws EryzBotException {
+        File file = new File(filepath);
+        if (!file.exists() || file.length() == 0) {
+            return new ArrayList<>();
+        }
+        try (var fin = new FileInputStream(filepath); var ois = new ObjectInputStream(fin)) {
+            return (ArrayList<Task>) ois.readObject();
+        } catch (Exception e) {
+            throw new EryzBotException("Couldn't fetch tasks.");
+        }
+    }
+
+    public void save(ArrayList<Task> tasks) throws EryzBotException {
+        File file = new File(filepath);
+        file.getParentFile().mkdirs();
+
+        try (var fout = new FileOutputStream(filepath); var oos = new ObjectOutputStream(fout)) {
+            oos.writeObject(tasks);
+        } catch (IOException e) {
+            throw new EryzBotException("Couldn't save tasks.");
+        }
+    }
+}
