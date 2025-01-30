55bca1cea73e9498aae945c38f782c27a6be4c2e
Gabriella <gabriella@u.nus.edu>
2025-01-30 18:33:05 +0800

    Add JavaDoc for tests

--- src/test/java/eryz/EventTaskTest.java
+++ src/test/java/eryz/EventTaskTest.java
@@ -1,17 +1,18 @@
 package eryz;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
 
 public class EventTaskTest {
 
     @Test
     public void testEventTaskCreateValidInput() {
         String input = "event meeting cs2103T /from 2025-02-01 /to 2025-02-02";
-        Task task = EventTask.EventTaskCreate(input);
+        Task task = EventTask.eventTaskCreate(input);
         assertNotNull(task);
         assertTrue(task instanceof EventTask);
     }
@@ -19,13 +20,13 @@ public class EventTaskTest {
     @Test
     public void testEventTaskCreateInvalidInput() {
         String input = "event meeting cs2103T /from 2025-02-01";
-        assertThrows(EryzBotException.class, () -> EventTask.EventTaskCreate(input));
+        assertThrows(EryzBotException.class, () -> EventTask.eventTaskCreate(input));
     }
 
     @Test
     public void testEventTaskPrint() {
         String input = "event meeting cs2103T /from 2025-02-01 /to 2025-02-02";
-        Task task = EventTask.EventTaskCreate(input);
+        Task task = EventTask.eventTaskCreate(input);
         task.printTask();
         assertDoesNotThrow(() -> task.printTask());
     }
