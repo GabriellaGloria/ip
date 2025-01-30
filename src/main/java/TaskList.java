f63bb4c6fce1e146b3b39d83e42650f5ca0c92b3
Gabriella <gabriella@u.nus.edu>
2025-01-30 15:19:09 +0800

    Add list and print function to TaskList

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class TaskList {
     private final ArrayList<Task> tasks;
-    
+
     TaskList() {
         this(new ArrayList<>());
     }
@@ -17,5 +17,17 @@ public class TaskList {
     }
     void unmark(int index) {
         tasks.get(index).unmark();
+    }
+    void printTask(int idx){
+        tasks.get(idx - 1).printTask();
+    }
+    void list(){
+        if (tasks.isEmpty()) {
+            System.out.println("You have no tasks. Yay!");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                printTask(i + 1);
+            }
+        }
     }
 }
